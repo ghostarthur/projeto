62ca17286b48c1254c564cd0260dbd5b3a290124
“ghostarthur” <iwiahajaha@gmail.com>
2023-09-17 14:57:05 -0300

    tudo feito

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 function toggleMode() {
   const html = document.documentElement
-  html.classList.toggle("ligth")
+  html.classList.toggle("light")
 
   //pegar a tag img
   const img = document.querySelector("#profile img")
